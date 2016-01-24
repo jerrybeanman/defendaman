b3090e2c8cd5361a1ec84aafdb76423c6d4e6381
jerrybeanman <jerry1bean@hotmail.com>
2016-01-23 22:37:42 -0800

    added a couple basi scripts. and a simple class heirachy

---
+++ Assets/Scripts/Classes/BaseClass.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseClass{
+
+	/* Name of the class. Ex: "Archer, warrior.." */
+	private string _className;
+
+	/* Short description of the class.*/
+	private string _classDescription;
+
+	/* Base stats that all classes share*/
+	private PlayerBaseStat _classStat;
+	
+	
+	public string ClassName
+	{
+		get { return this._className; }
+		set { this._className = value;}
+	}
+
+	public string ClassDescription
+	{
+		get { return this._classDescription; }
+		set { this._classDescription = value;}
+	}
+
+	private PlayerBaseStat ClassStat
+	{
+		get {return this._classStat; }
+		set
+		{
+			this._classStat.CurrentHp = value.CurrentHp;
+			this._classStat.MaxHp = value.MaxHp;
+			this._classStat.MoveSpeed = value.MoveSpeed;
+			this._classStat.AtkPower = value.AtkPower;
+		}
+	}
+
+
+
+
+	[System.Serializable]
+	public class PlayerBaseStat
+	{
+		public float CurrentHp;
+		public float MaxHp;
+		public float MoveSpeed;
+		public float AtkPower;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Player : MonoBehaviour {
+
+	
+	private Animator _animator;
+
+	public void Awake()
+	{
+		_animator = GetComponent<Animator>();
+		PlayerEvents.OnHit += this.OnHit;
+		PlayerEvents.OnMove += this.OnMove;
+		
+	}
+
+	public void Destroy()
+	{
+		PlayerEvents.OnHit -= this.OnHit;
+		PlayerEvents.OnMove -= this.OnMove;
+		
+	}
+
+	public void OnHit()
+	{
+
+	}
+
+	public void OnMove()
+	{
+
+	}
+
+
+}
