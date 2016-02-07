7cfa885beebf958b6bee83597f676d442ef54113
Jerry Jia <jiajerry2516@gmail.com>
2016-02-07 01:19:40 -0800

    Update coding_conventions.c
    Fixed indentations and commenting

--- coding_conventions.c
+++ coding_conventions.c
@@ -1,8 +1,9 @@
-/*----------------------------------
-		Coding Conventions: 
- -----------------------------------*/
+/******************************************
+		Coding Conventions: 		
+ ******************************************/
 
-/* Notes:
+/*********************************************************************************************************************************** 
+Notes:
 	-Don't throw in extra comments that you don't need. Put comments at places that you think that people might not understand.
 		too much comments can make code base crowded and unreadable. 
 
@@ -12,32 +13,33 @@
 	-Make code blocks readable, try not to crowd everything in one chunk, leave extra new lines between function calls.	
 
 	-Modularize functions, dont make 1 giant function. Break it up for readers to take a break.
-*/
+*************************************************************************************************************************************/
 
 
-/* descriptive / consistent variable names 
-		ex: A variable that contains the player's movement speed 
-*/
+/************************************************************************
+descriptive / consistent variable names 
+	ex: A variable that contains the player's movement speed 
+************************************************************************ */
 int pms; 			// bad
-int p_move_speed;	// good
+int p_move_speed;		// good
 
 
-/* 
-   private variables declared with a "_" in the front
-*/
+/********************************************************
+private variables declared with a "_" in the front
+*********************************************************/
 private Transform  _transform;
 
 
-/* 
-  	descriptive / consistent function names (especially for API calls)
-  		ex: Function that retrieves data packets from server
-*/
-public SomeStruct get_p(); 					// needs to make it more explicit
+/********************************************************************** 
+descriptive / consistent function names (especially for API calls)
+	ex: Function that retrieves data packets from server
+***********************************************************************/
+public SomeStruct get_p(); 			// needs to make it more explicit
 public SomeStruct RetriveServerPackets();	// easier to call from an IDE (Auto complete). 
 
-/* 
-   private functions declared with lowercase seperated with "_"
-   public functions declared Uppcaes for every word
-*/
-private void attemp_move();					// see if the player can move, check for blockables in its path
-public void GetPlayerPosition();			// retrieves the 2D vector of where the player is currently at
+/****************************************************************
+private functions declared with lowercase seperated with "_"
+public functions declared Uppcaes for every word
+*****************************************************************/
+private void attemp_move();			// see if the player can move, check for blockables in its path
+public void GetPlayerPosition();		// retrieves the 2D vector of where the player is currently at
