90d9fcd439ed48987105360b8a842826b522be34
JaegarSarauer <jaegars@hotmail.com>
2016-02-21 12:24:28 -0800

    Working map with generation from data passed in from Carson's networking manager. The JSON object that is parsed isn't passed from the server yet, but just on the creation of the manager.

---
+++ Assets/Scripts/Map/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    private GameObject Player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (Player != null)
+        Player = GameObject.Find("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Player.transform.position.x, 10, Player.transform.position.z);
+    }
+}
