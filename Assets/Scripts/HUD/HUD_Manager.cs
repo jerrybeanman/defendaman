c38ee5ff8f64e3a7163d4e70668f3ac8b4b08030
jerrybeanman <jerry1bean@hotmail.com>
2016-03-01 23:25:02 -0800

    added ally and enemy king + currency UI display, with animaions and Wrapper functions.

---
+++ Assets/Scripts/HUD/HUD_Manager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+using System.Globalization;
+
+public class HUD_Manager : MonoBehaviour {
+	[System.Serializable]
+	public class PlayerProfile 	{ public Image Health;		public Animator HealthAnimator; }
+	[System.Serializable]
+	public class AllyKing 		{ public Image Health;		public Animator HealthAnimator; }
+	[System.Serializable]
+	public class EnemyKing 		{ public Image Health;		public Animator HealthAnimator; }
+	[System.Serializable]
+	public class Currency 		{ public Text  Amount;		public Animator CurrencyAnimator; }
+
+	public static HUD_Manager 	instance; 
+
+	public Currency				currency;
+	public PlayerProfile 		playerProfile;
+	public AllyKing				allyKing;
+	public EnemyKing			enemyKing;
+
+	void Awake()
+	{
+		if (instance == null)				//Check if instance already exists
+			instance = this;				//if not, set instance to this
+		else if (instance != this)			//If instance already exists and it's not this:
+			Destroy(gameObject);   			//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager. 
+		DontDestroyOnLoad(gameObject);		//Sets this to not be destroyed when reloading scene
+	}
+
+	public void PlayerTakeDmg(float Dmg)
+	{
+		playerProfile.Health.fillAmount -= Dmg;
+		playerProfile.HealthAnimator.SetTrigger("TakeDmg");
+	}
+
+	public void AllyKingTakeDmg(float Dmg)
+	{
+		allyKing.Health.fillAmount -= Dmg;
+		allyKing.HealthAnimator.SetTrigger("TakeDmg");
+	}
+
+	public void EnemyKingTakeDmg(float Dmg)
+	{
+		enemyKing.Health.fillAmount -= Dmg;
+		enemyKing.HealthAnimator.SetTrigger("TakeDmg");
+	}
+
+	public void UpdateCurrency(int amount)
+	{
+		int num = int.Parse(currency.Amount.text, NumberStyles.AllowThousands) + amount;
+		currency.Amount.text = String.Format("{0:n0}", num);
+		currency.CurrencyAnimator.SetTrigger("UpdateCurrency");
+	}
+}
---
+++ Assets/Scripts/HUD/HudTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HudTest : MonoBehaviour {
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown("1"))
+			HUD_Manager.instance.PlayerTakeDmg(0.05f);
+		if(Input.GetKeyDown("2"))
+			HUD_Manager.instance.AllyKingTakeDmg(0.05f);
+		if(Input.GetKeyDown("3"))
+			HUD_Manager.instance.EnemyKingTakeDmg(0.05f);
+		if(Input.GetKeyDown("4"))
+			HUD_Manager.instance.UpdateCurrency(100);
+	}
+}
