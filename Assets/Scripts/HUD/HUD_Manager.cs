e64855fab320d5d3a09539acf9a99aac0eaf3c5a
jerrybeanman <jerry1bean@hotmail.com>
2016-03-02 00:58:02 -0800

    completed skill UI system, no animations yet

--- Assets/Scripts/HUD/HUD_Manager.cs
+++ Assets/Scripts/HUD/HUD_Manager.cs
@@ -13,13 +13,22 @@ public class HUD_Manager : MonoBehaviour {
 	public class EnemyKing 		{ public Image Health;		public Animator HealthAnimator; }
 	[System.Serializable]
 	public class Currency 		{ public Text  Amount;		public Animator CurrencyAnimator; }
-
+	[System.Serializable]
+	public class MainSkill 		{ public Image ProgressBar;	public float CoolDown; }
+	[System.Serializable]
+	public class SubSkill 		{ public Image ProgressBar;	public float CoolDown; }
+	[System.Serializable]
+	public class PassiveSkill 	{ public Image ProgressBar;	public float CoolDown; }
+	
 	public static HUD_Manager 	instance; 
 
 	public Currency				currency;
 	public PlayerProfile 		playerProfile;
 	public AllyKing				allyKing;
 	public EnemyKing			enemyKing;
+	public MainSkill			mainSkill;
+	public SubSkill				subSkill;
+	public PassiveSkill			passiveSkill;
 
 	void Awake()
 	{
@@ -30,21 +39,36 @@ public class HUD_Manager : MonoBehaviour {
 		DontDestroyOnLoad(gameObject);		//Sets this to not be destroyed when reloading scene
 	}
 
-	public void PlayerTakeDmg(float Dmg)
+	void Update()
 	{
-		playerProfile.Health.fillAmount -= Dmg;
+		mainSkill.ProgressBar.fillAmount = Mathf.Lerp(0f, 1f, mainSkill.ProgressBar.fillAmount);
+		if(mainSkill.ProgressBar.fillAmount  < 1)
+			mainSkill.ProgressBar.fillAmount  += Time.deltaTime / mainSkill.CoolDown;
+
+		subSkill.ProgressBar.fillAmount = Mathf.Lerp(0f, 1f, subSkill.ProgressBar.fillAmount);
+		if(subSkill.ProgressBar.fillAmount < 1)
+			subSkill.ProgressBar.fillAmount += Time.deltaTime / subSkill.CoolDown;
+
+		passiveSkill.ProgressBar.fillAmount = Mathf.Lerp(0f, 1f, passiveSkill.ProgressBar.fillAmount);
+		if(passiveSkill.ProgressBar.fillAmount < 1)
+			passiveSkill.ProgressBar.fillAmount += Time.deltaTime / passiveSkill.CoolDown;
+	}
+
+	public void UpdatePlayerHealth(float Dmg)
+	{
+		playerProfile.Health.fillAmount += Dmg;
 		playerProfile.HealthAnimator.SetTrigger("TakeDmg");
 	}
 
-	public void AllyKingTakeDmg(float Dmg)
+	public void UpdateAllyKingHealth(float Dmg)
 	{
-		allyKing.Health.fillAmount -= Dmg;
+		allyKing.Health.fillAmount += Dmg;
 		allyKing.HealthAnimator.SetTrigger("TakeDmg");
 	}
 
-	public void EnemyKingTakeDmg(float Dmg)
+	public void UpdateEnemyKingHealth(float Dmg)
 	{
-		enemyKing.Health.fillAmount -= Dmg;
+		enemyKing.Health.fillAmount += Dmg;
 		enemyKing.HealthAnimator.SetTrigger("TakeDmg");
 	}
 
@@ -53,5 +77,32 @@ public class HUD_Manager : MonoBehaviour {
 		int num = int.Parse(currency.Amount.text, NumberStyles.AllowThousands) + amount;
 		currency.Amount.text = String.Format("{0:n0}", num);
 		currency.CurrencyAnimator.SetTrigger("UpdateCurrency");
+	}
+
+	public void UseMainSkill(float CoolDown)
+	{
+		if(subSkill.ProgressBar.fillAmount >= 1)
+		{
+			mainSkill.ProgressBar.fillAmount = 0f;
+			mainSkill.CoolDown = CoolDown;
+		}
+	}
+
+	public void UseSubSkill(float CoolDown)
+	{
+		if(subSkill.ProgressBar.fillAmount >= 1)
+		{
+			subSkill.ProgressBar.fillAmount = 0f;
+			subSkill.CoolDown = CoolDown;
+		}
+	}
+
+	public void UsePassive(float Cooldown)
+	{
+		if(passiveSkill.ProgressBar.fillAmount >= 1)
+		{
+			passiveSkill.ProgressBar.fillAmount = 0f;
+			passiveSkill.CoolDown = Cooldown;
+		}
 	}
 }
