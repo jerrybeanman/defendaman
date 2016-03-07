cc0238af4113648fe945f8644d930c7ee6a3032b
jerrybeanman <jerry1bean@hotmail.com>
2016-03-06 21:33:56 -0800

    Aded json parsing on lobby

--- Assets/Scripts/GameManagers/Networking/LobbyData.cs
+++ Assets/Scripts/GameManagers/Networking/LobbyData.cs
@@ -5,17 +5,18 @@ using System.Text;
 
 public enum ClassType { Gunner = 1, Ninja = 2, Wizard = 3, NotImplemented = 4}
 
-public struct PlayerData {
+public class PlayerData {
     public int PlayerID { get; set; }
     public string Username { get; set; }
     public int TeamID { get; set; }
     public ClassType ClassType { get; set; }
+	public bool Ready { get; set; }
     //public bool King { get; set; }
 }
 
 public class GameData
 {
-    public static List<PlayerData> LobbyData = new List<PlayerData>();
+    public static Dictionary<int,PlayerData> LobbyData = new Dictionary<int, PlayerData>();
     public static int MyPlayerID { get; set; }
     public static int EnemyKingID { get; set; }
     public static int AllyKingID { get; set; }
