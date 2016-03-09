05817b63d1814b097c20a8c2beea4712183e9cdb
Hank Lo <Hlo1453@gmail.com>
2016-03-09 11:15:54 -0800

    Applied buff system to Wizard class Magic circle

--- Assets/Scripts/Classes/WizardClass.cs
+++ Assets/Scripts/Classes/WizardClass.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class WizardClass : BaseClass
 {
-    // note that max distance for the special attack is meant to be the duration that the magic circle stays 
-    int[] distance = new int[2]{ 10, 100};
+    // note that max distance for this special attack is meant to be the duration that the magic circle stays 
+    int[] distance = new int[2]{ 10, 0 };
     int[] speed = new int[2] { 100, 0 };
     Rigidbody2D fireball = (Rigidbody2D)Resources.Load("Prefabs/Bullet", typeof(Rigidbody2D));
     Rigidbody2D magicCircle = (Rigidbody2D)Resources.Load("Prefabs/magic_circle", typeof(Rigidbody2D));
@@ -17,19 +17,28 @@ public class WizardClass : BaseClass
         this._classStat.MaxHp = 150;
 
         //placeholder numbers
-        this._classStat.MoveSpeed = 10;
+        this._classStat.MoveSpeed = 2;
         this._classStat.AtkPower = 15;
 
         var controller = Resources.Load("Controllers/magegirl") as RuntimeAnimatorController;
         gameObject.GetComponent<Animator>().runtimeAnimatorController = controller;
-        cooldowns = new float[2] { 0.5f, 2 };
+        cooldowns = new float[2] { 0.5f, 6 };
 	}
 
     //attacks return time it takes to execute
     public override float basicAttack(Vector2 dir)
     {
-        base.basicAttack(dir);
-        return cooldowns[0];
+        //base.basicAttack(dir);
+        // temp
+        /*
+        Rigidbody2D attack = (Rigidbody2D)Instantiate(bullet, transform.position, transform.rotation);
+        attack.AddForce(dir * speed[0]);
+        attack.GetComponent<BasicRanged>().teamID = team;
+        attack.GetComponent<BasicRanged>().damage = ClassStat.AtkPower;
+        attack.GetComponent<BasicRanged>().maxDistance = distance[0];
+        */
+
+        return 0; //cooldowns[0];
     }
 
     public override float specialAttack(Vector2 dir)
@@ -43,7 +52,7 @@ public class WizardClass : BaseClass
         Rigidbody2D attack = (Rigidbody2D)Instantiate(magicCircle, mousePos, Quaternion.identity);
         attack.GetComponent<MagicCircle>().teamID = team;
         attack.GetComponent<MagicCircle>().damage = ClassStat.AtkPower * 0;
-        attack.GetComponent<MagicCircle>().maxDistance = distance[1];
+        attack.GetComponent<MagicCircle>().duration = 200;
 
         return cooldowns[1];
     }
--- Assets/Scripts/MagicCircle.cs
+++ Assets/Scripts/MagicCircle.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 using System;
 
-public class MagicCircle : Projectile
+public class MagicCircle : Area
 {
     private Vector2 startPos;
-    public int maxDistance;
+    public int duration;
 
     //projectile speed
 
@@ -14,30 +14,64 @@ public class MagicCircle : Projectile
         startPos = transform.position;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         // this calculates how long the circle stays for
-        maxDistance--;
-        if (maxDistance <= 0)
+        if (--duration < 0)
         {
             Destroy(gameObject);
         }
     }
 
-    protected override void OnTriggerEnter2D(Collider2D other)
+    protected void OnTriggerStay2D(Collider2D other) 
     {
+        // if same team
         if (other.gameObject.GetComponent<BaseClass>() != null && teamID == other.gameObject.GetComponent<BaseClass>().team)
         {
-            //gameObject.AddComponent("MagicBuff");
+            var b = other.gameObject.GetComponent<MagicBuff>();
+            if (b == null)
+            {
+                other.gameObject.AddComponent<MagicBuff>();
+            }
+            else
+            {
+                b.duration = 150;
+            }
+            return;
+        } 
+        else if (other.gameObject.GetComponent<BaseClass>() != null && teamID != other.gameObject.GetComponent<BaseClass>().team)
+        {
+            var db = other.gameObject.GetComponent<MagicDebuff>();
+            if (db == null)
+            {
+                other.gameObject.AddComponent<MagicDebuff>();
+            }
+            else
+            {
+                db.duration = 150;
+            }
             return;
         }
     }
 
-    protected void onTriggerStay2D(Collider2D other) 
-    {
+    protected void OnTriggerExit2D(Collider2D other) {
+        // if same team
         if (other.gameObject.GetComponent<BaseClass>() != null && teamID == other.gameObject.GetComponent<BaseClass>().team)
         {
-            //gameObject.AddComponent("MagicBuff");
+            var b = other.gameObject.GetComponent<MagicBuff>();
+            if (b != null)
+            {
+                b.duration = -1;
+            }
+            return;
+        } 
+        else if (other.gameObject.GetComponent<BaseClass>() != null && teamID != other.gameObject.GetComponent<BaseClass>().team)
+        {
+            var db = other.gameObject.GetComponent<MagicDebuff>();
+            if (db != null)
+            {
+                db.duration = -1;
+            }
             return;
         }
     }
