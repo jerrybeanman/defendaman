8e438d89a034de3b3cae5bd5615f8a1b39ee1cbd
Hank Lo <Hlo1453@gmail.com>
2016-03-17 13:06:28 -0700

    changed MC to use a dir based instantiation

--- Assets/Scripts/Classes/WizardClass.cs
+++ Assets/Scripts/Classes/WizardClass.cs
@@ -83,7 +83,8 @@ public class WizardClass : RangedClass
     --
     -- DATE: March 9, 2016
     --
-    -- REVISIONS: None
+    -- REVISIONS:
+    --      - March 17, 2016: Fixed instantiation to work through networking
     --
     -- DESIGNER: Hank Lo
     --
@@ -100,11 +101,14 @@ public class WizardClass : RangedClass
     public override float specialAttack(Vector2 dir)
     {
         base.specialAttack(dir);
+
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = 10.0f;
         mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        var distance = mousePos - transform.position;
+        var endp = transform.position + (distance * dir);
 
-        Rigidbody2D attack = (Rigidbody2D)Instantiate(magicCircle, mousePos, Quaternion.identity);
+        Rigidbody2D attack = (Rigidbody2D)Instantiate(magicCircle, endp, Quaternion.identity);
         attack.GetComponent<MagicCircle>().playerID = playerID;
         attack.GetComponent<MagicCircle>().teamID = team;
         attack.GetComponent<MagicCircle>().damage = ClassStat.AtkPower * 0;
