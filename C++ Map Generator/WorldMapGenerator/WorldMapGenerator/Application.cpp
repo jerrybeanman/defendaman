ad516cd7ae10648349d9f496a406e7d4e27756a2
JaegarSarauer <jaegars@hotmail.com>
2016-03-17 14:14:11 -0700

    Scenery Layer added

--- C++ Map Generator/WorldMapGenerator/WorldMapGenerator/Application.cpp
+++ C++ Map Generator/WorldMapGenerator/WorldMapGenerator/Application.cpp
@@ -17,30 +17,20 @@ void drawMap (int ** mapArray, Map map) {
 
 int main () {
 	char c;
-	Map *map = new Map (50, 50, 24);
+	Map *map = new Map (10, 10, 24);
 	do {
-		map->randomizeSeed ();
+		map->randomizeSeed();
 		map->buildMapBase ();
 		map->createBaseScenery ();
 		map->joinMaps (map->getMapBase (), map->getMapTemp ());
+		map->createTopScenery (10);
 		map->createSpawnPoints (map->getMapBase (), 2);
-		printf("%s", map->ConvertToJSONString (map->getMapBase ()).c_str());
-		//drawMap (map->getMapBase (), *map);
+		//printf("%s", map->ConvertToJSONString (map->getMapBase ()).c_str());
+		drawMap (map->getMapBase (), *map);
 		printf ("\n\nEnter 'q' to quit, any other key will regernate.\n\n");
 		c = getchar ();
-		Map* map2 = new Map (100, 100, 44);
+		Map* map2 = new Map (10, 10, 24);
 		map = map2;
 	} while (c != 'q');
 	return 0;
-}
-
-
-const char * GenerateMap (int seed) {
-	Map *map = new Map (50, 50, 24);
-	map->mapSeed = seed;
-	map->buildMapBase ();
-	map->createBaseScenery ();
-	map->joinMaps (map->getMapBase (), map->getMapTemp ());
-	map->createSpawnPoints (map->getMapBase (), 2);
-	return (map->ConvertToJSONString (map->getMapBase ()).c_str ());
 }
--- C++ Map Generator/WorldMapGenerator/WorldMapGenerator/MapGenerationLibrary.cpp
+++ C++ Map Generator/WorldMapGenerator/WorldMapGenerator/MapGenerationLibrary.cpp
@@ -1,14 +1,21 @@
 #define STRICT
 #include <stdio.h>
 #include <string>
+#include <string.h>
 #include "Map.h"
 
+char endMap[50000];
+
 extern "C" const char * GenerateMap (int seed) {
-	Map *map = new Map (50, 50, 24);
-	map->mapSeed = seed;
+	Map *map = new Map (100, 100, 44);
+	map->randomizeSeed (seed);
 	map->buildMapBase ();
 	map->createBaseScenery ();
 	map->joinMaps (map->getMapBase (), map->getMapTemp ());
+
+	//first int is 1:N chance of creat
+	map->createTopScenery (10);
 	map->createSpawnPoints (map->getMapBase (), 2);
-	return (map->ConvertToJSONString (map->getMapBase ()).c_str ());
+	strcpy (endMap, map->ConvertToJSONString ().c_str ());
+	return (endMap);
 }
