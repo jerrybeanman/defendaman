b61d1bde4bfa2d5a1fd035207e965cbd658a9be0
Carson Roscoe <CarsonRoscoe7@gmail.com>
2016-03-17 20:52:57 -0700

    Fixed another inventory bug with Joseph

--- Assets/Scripts/HUD/InventorySystem/WorldItemManager.cs
+++ Assets/Scripts/HUD/InventorySystem/WorldItemManager.cs
@@ -104,9 +104,9 @@ public class WorldItemManager : MonoBehaviour
         // if (_my_player_id == player_id) // Disabled for testing, the player ID is set later
         if (GameData.MyPlayer.PlayerID == player_id)
         {
-            CreateWorldItem(world_item_id, item_id, amt, pos_x, pos_y);
+            _inventory.DestroyInventoryItem(inv_pos, amt);
         }
-        _inventory.DestroyInventoryItem(inv_pos, amt);
+        CreateWorldItem(world_item_id, item_id, amt, pos_x, pos_y);
     }
 
     /*
