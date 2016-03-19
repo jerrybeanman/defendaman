586807558832d84d89ecedd734455e1a8711f9e5
Micah <micah0willems@gmail.com>
2016-03-18 18:30:02 -0700

    Added hidden enemies & items functionality

--- Assets/Scripts/HUD/InventorySystem/WorldItemData.cs
+++ Assets/Scripts/HUD/InventorySystem/WorldItemData.cs
@@ -38,6 +38,10 @@ public class WorldItemData : MonoBehaviour
      */
     void Start ()
     {
+		gameObject.layer = LayerMask.NameToLayer ("HiddenThings");
+		gameObject.transform.Translate(0,0,9);
+		// set material to stencil masked
+		gameObject.GetComponent<SpriteRenderer> ().material = (Material)Resources.Load("Stencil_01_Diffuse Sprite", typeof(Material));
         _player_id = GameData.MyPlayer.PlayerID;
         _world_item_manager = GameObject.Find("GameManager").GetComponent<WorldItemManager>();
         _tooltip = GameObject.Find("Inventory").GetComponent<Tooltip>();
