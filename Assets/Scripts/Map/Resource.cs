82ab39ad22dc66deed2625d1ae98d24b121633ea
JaegarSarauer <jaegars@hotmail.com>
2016-03-23 14:44:11 -0700

    Resource object list created on map load

---
+++ Assets/Scripts/Map/Resource.cs
@@ -0,0 +1,18 @@
+﻿class Resource {
+    public int x;
+    public int y;
+    public int resourceLeft;
+    
+
+    public Resource(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.resourceLeft = 10;
+    }
+
+    public Resource(int x, int y, int ra) {
+        this.x = x;
+        this.y = y;
+        this.resourceLeft = ra;
+    }
+}
