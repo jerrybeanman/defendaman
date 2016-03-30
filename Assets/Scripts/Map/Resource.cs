0ace9e3ca89346b2af3c9ae8179ca19d68d7f5f8
Krystle Bulalakaw <krystle.bulalakaw@gmail.com>
2016-03-30 16:47:25 -0700

    Drop gold on player attack triggering the tree collision box

--- Assets/Scripts/Map/Resource.cs
+++ Assets/Scripts/Map/Resource.cs
@@ -20,8 +20,10 @@ class Resource : MonoBehaviour {
 	public int x {get; set;}
 	public int y {get; set;}
 	public int amount {get; set;}
-	Animator animator;
-	CircleCollider2D mollider;
+	public int instanceId {get; set;}
+	public bool trigger_entered {get; set;}
+	public bool depleted {get; set;}
+	Animator animator {get; set;}
 
     public Resource(int x, int y) {
         this.x = x;
@@ -48,7 +50,6 @@ class Resource : MonoBehaviour {
     ----------------------------------------------------------------------------------------------------------------------*/
 	void Start () {
 		animator = GetComponent<Animator>();
-		mollider = GetComponent<CircleCollider2D>(); 
 	}
 
 	/*------------------------------------------------------------------------------------------------------------------
@@ -77,43 +78,77 @@ class Resource : MonoBehaviour {
     -- RETURNS: 	void.
     -- NOTES:
     -- Decreases the amount of a resource object by some number.
+    -- On resource depletion, start a coroutine to play the explosion animation then destroy the object.
     ----------------------------------------------------------------------------------------------------------------------*/
 	void DecreaseAmount(int amount) {
 		string amt1, amt2;
 		amt1 = this.amount.ToString();
 		this.amount -= amount;
 
-		amt2 = this.amount.ToString();
-		Debug.Log("Decreased resource amount from " + amt1 + " to " + amt2 + " at (" + this.x + ", " + this.y + ")" );
+		DropGold (amount);
 
 		if (this.amount <= 0) {
 			this.amount = 0;
-			animator.SetTrigger("Depleted");
-			mollider.isTrigger = true;
-			Debug.Log ("Resource at (" + this.x + ", " + this.y + ") depleted" );
+			depleted = true;
+			StartCoroutine(ExplodeAndDestroy());
 		}
+		
+		Debug.Log("Decreased resource amount from " + amt1 + " to " + this.amount + " at (" + this.x + ", " + this.y + ")" );
 	}
 
 	/*------------------------------------------------------------------------------------------------------------------
-    -- FUNCTION: 	DecreaseAmount
-    -- DATE: 		March 24, 2016
+    -- FUNCTION: 	ExplodeAndDestroy
+    -- DATE: 		March 30, 2016
     -- REVISIONS: 	N/A
     -- DESIGNER:  	Krystle Bulalakaw
     -- PROGRAMMER: 	Krystle Bulalakaw
-    -- INTERFACE: 	DecreaseAmount(int amount)
-    --					int amount: the remaining quantity of the resource object on the map
+    -- INTERFACE: 	ExplodeAndDestory()
+    -- RETURNS: 	IEnumerator - generic enumerator used to run a coroutine alongside Update()
+    -- NOTES:
+    -- Plays the explosion animation on resource depletion, waits for animation to complete, then destroys the gameobject.
+    ----------------------------------------------------------------------------------------------------------------------*/
+	IEnumerator ExplodeAndDestroy() {
+		animator.SetTrigger("Depleted");
+		// TODO: get animation clip length
+		yield return new WaitForSeconds(0.65f);
+		Destroy(gameObject);
+	}
+
+	/*------------------------------------------------------------------------------------------------------------------
+    -- FUNCTION: 	OnTriggerEnter2D
+    -- DATE: 		March 30, 2016
+    -- REVISIONS: 	N/A
+    -- DESIGNER:  	Krystle Bulalakaw
+    -- PROGRAMMER: 	Krystle Bulalakaw
+    -- INTERFACE: 	OnTriggerEnter2D(Collider2D other)
+    --					Collider2D other - the object that triggered the collision box
     -- RETURNS: 	void.
     -- NOTES:
-    -- Triggered when the resource object's Collision2D detects collision with another Collision2D object.
-    -- If the player interacted to claim the resource, decrease the resource amount by some number.
+    -- Triggered when the resource object's collision box is triggered by a Collider 2D object (player attack)
+    -- Decreasea the resource amount by some number.
     ----------------------------------------------------------------------------------------------------------------------*/
-	void OnCollisionStay2D(Collision2D other) {
-		//Debug.Log ("Resource collision other: " + other.collider.name);
-		if (other.collider.name.Contains("Player")) {
-			if (Input.GetKeyUp("e")) {
-				DecreaseAmount(1);
-			}
-		}
+	void OnTriggerEnter2D(Collider2D other) {
+		DecreaseAmount(1);
+	}
+
+	/*------------------------------------------------------------------------------------------------------------------
+    -- FUNCTION: 	DropGold
+    -- DATE: 		March 30, 2016
+    -- REVISIONS: 	N/A
+    -- DESIGNER:  	Krystle Bulalakaw
+    -- PROGRAMMER: 	Krystle Bulalakaw
+    -- INTERFACE: 	DropGold(int amount)
+    --					int amount - amount of gold to drop
+    -- RETURNS: 	void.
+    -- NOTES:
+    -- Creates a gold world item with some amount.
+    -- Its X and Y position is offset so that it doesn't drop in the same spot every time, and so it is easier to pick
+    -- up (not right in the center of the tree).
+    ----------------------------------------------------------------------------------------------------------------------*/
+	private void DropGold(int amount) {
+		float offsetX = Random.Range (-1.0f, 1.0f);
+		float offsetY = Random.Range (-1.0f, 1.0f);
+		// Create Gold world item with playerId = 100 at this resource's X and Y
+		WorldItemManager.Instance.CreateWorldItem(100, 2, amount, x + offsetX, y + offsetY);
 	}
 }
-
