5595c87ae174a1b28bcb7394d06f2984c3abecd3
jerrybeanman <jerry1bean@hotmail.com>
2016-03-31 21:35:35 -0700

    different sprite sheet for enemy and ally

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -3,18 +3,25 @@ using System.Collections;
 
 public class HealthBar : MonoBehaviour
 {
-    public int maxHealth = 100;
-    public int curHealth = 100;
-    BaseClass baseClass;
 
-    public float healthBarLength;
-
+	private SpriteRenderer spriteRenderer;
+	public Sprite allyHealth;
+	public Sprite enemyHealth;
     // Use this for initialization
     void Start()
     {
-        healthBarLength = Screen.width / 6;
-        baseClass = GetComponent<BaseClass>();
-    }
+		// this is bad, dont do it lol. ill fix it later
+		spriteRenderer = transform.GetChild(0).transform.GetChild(0).GetComponent<SpriteRenderer>();
+
+		// fix this later too
+		if(transform.parent.gameObject.GetComponent<BaseClass>().team == GameData.MyPlayer.TeamID)
+		{
+			spriteRenderer.sprite = allyHealth;
+		}else
+		{
+			spriteRenderer.sprite = enemyHealth;
+		}
+	}
 
 
 	void LateUpdate()
@@ -22,29 +29,10 @@ public class HealthBar : MonoBehaviour
 		transform.rotation = Quaternion.Euler(0, 0, 0);
 		transform.position = new Vector3(transform.parent.position.x, transform.parent.position.y + 1, -10);
 	}
-    void OnGUI()
+
+
+    public void TakeDmg(float dmg)
     {
 
-        // Vector2 targetPos = transform.position;
-        //targetPos = Camera.main.WorldToScreenPoint(transform.position);
-
-        //GUI.Box(new Rect(targetPos.x -30, Screen.height - targetPos.y - 46, 60, 20), curHealth + "/" + maxHealth);
-
-    }
-
-    public void AddjustCurrentHealth(int adj)
-    {
-        curHealth += adj;
-
-        if (curHealth < 0)
-            curHealth = 0;
-
-        if (curHealth > maxHealth)
-            curHealth = maxHealth;
-
-        if (maxHealth < 1)
-            maxHealth = 1;
-
-        healthBarLength = (Screen.width / 6) * (curHealth / (float)maxHealth);
     }
 }
