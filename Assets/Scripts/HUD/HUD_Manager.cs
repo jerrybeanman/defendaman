3bd231094e12fb69c49cd55238cd6329540f5e86
Micah <micah0willems@gmail.com>
2016-04-01 18:02:24 -0700

    Upgrade menu bugs fixed

--- Assets/Scripts/HUD/HUD_Manager.cs
+++ Assets/Scripts/HUD/HUD_Manager.cs
@@ -817,4 +817,9 @@ public class HUD_Manager : MonoBehaviour {
 	{
 		Debug.Log ("Upgrade weapon here");
 	}
+	public void AddPotion(String potionType)
+	{
+		Debug.Log (potionType + " Potion");
+		Debug.Log ("Add potion here");
+	}
 }
