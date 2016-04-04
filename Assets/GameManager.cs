8e400ff31d34db0ceabdd0d737be5788ee326430
Micah <micah0willems@gmail.com>
2016-04-04 11:51:19 -0700

    Changed vision to be different per class

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -10,11 +10,35 @@ public class GameManager : MonoBehaviour {
     public static GameManager instance;
     public Transform AI;
 
+	[System.Serializable]
+	public class GunnerVision
+	{
+		public Transform lightSourceFOV;
+		public Transform hiderFOV;
+		public Transform lightSourcePeripheral;
+		public Transform hiderPeripheral;
+	}
+
+	[System.Serializable]
+	public class NinjaVision
+	{
+		public Transform lightSourceFOV;
+		public Transform hiderFOV;
+		public Transform lightSourcePeripheral;
+		public Transform hiderPeripheral;
+	}
+
+	[System.Serializable]
+	public class MageVision
+	{
+		public Transform lightSourcePeripheral;
+		public Transform hiderPeripheral;
+	}
+
+	public GunnerVision gunnerVision;
+	public NinjaVision ninjaVision;
+	public MageVision mageVision;
     public Transform playerType;
-    public Transform lightSourceFOV;
-	public Transform hiderFOV;
-	public Transform lightSourcePeripheral;
-	public Transform hiderPeripheral;
 	public Transform shadowoverlay;
     public GameObject player;
     private bool testing = false;
@@ -132,8 +156,6 @@ public class GameManager : MonoBehaviour {
                 myTeam = playerData.Value.TeamID;
 				player = createdPlayer;
 				GameObject.Find("Main Camera").GetComponent<FollowCamera>().target = player.transform;
-//				GameObject.Find("Camera FOV").GetComponent<FollowCamera>().target = player.transform;
-//				GameObject.Find("Camera Enemies").GetComponent<FollowCamera>().target = player.transform;
 				if (GameObject.Find("Minimap Camera") != null)
 					GameObject.Find("Minimap Camera").GetComponent<FollowCamera>().target = player.transform;
 				player.AddComponent<Movement>();
@@ -150,42 +172,115 @@ public class GameManager : MonoBehaviour {
             if (myTeam == playerData.Value.TeamID)
             {
 				// Get the player/teammember's hpFrame and bar
-                Transform hpFrame = createdPlayer.transform.GetChild(0);
-                Transform hpBar = hpFrame.transform.GetChild(0);
-                createdPlayer.layer = LayerMask.NameToLayer("Allies");
-                hpFrame.gameObject.layer = LayerMask.NameToLayer("Allies");
-                hpBar.gameObject.layer = LayerMask.NameToLayer("Allies");
-				// These are the FOV & peripheral vision occlusion masks
-                var lightingFOV = ((Transform)Instantiate(lightSourceFOV, createdPlayer.transform.position, Quaternion.identity)).gameObject;
-				lightingFOV.transform.parent = createdPlayer.transform;
-                lightingFOV.GetComponent<LightFollowPlayer>().target = createdPlayer.transform;
-                lightingFOV.GetComponent<RotateWithPlayer>().target = createdPlayer.transform;
-                lightingFOV.transform.Translate(0, 0, 8);
+				Transform hpFrame = createdPlayer.transform.GetChild(0);
+				Transform hpBar = hpFrame.transform.GetChild(0);
+				createdPlayer.layer = LayerMask.NameToLayer("Allies");
+				hpFrame.gameObject.layer = LayerMask.NameToLayer("Allies");
+				hpBar.gameObject.layer = LayerMask.NameToLayer("Allies");
 
-                var lightingPeripheral = ((Transform)Instantiate(lightSourcePeripheral, createdPlayer.transform.position, Quaternion.identity)).gameObject;
-				lightingPeripheral.transform.parent = createdPlayer.transform;
-                lightingPeripheral.GetComponent<LightFollowPlayer>().target = createdPlayer.transform;
-                lightingPeripheral.transform.Translate(0, 0, 8);
+				switch (GameData.LobbyData[playerData.Value.PlayerID].ClassType) {
+				case ClassType.Gunner:
 
-				// These are the FOV & peripheral vision stencil masks
-				var hiderLayerFOV = ((Transform)Instantiate(hiderFOV, createdPlayer.transform.position, Quaternion.identity)).gameObject;
-				hiderLayerFOV.transform.parent = createdPlayer.transform;
-				hiderLayerFOV.GetComponent<LightFollowPlayer>().target = createdPlayer.transform;
-				hiderLayerFOV.GetComponent<RotateWithPlayer>().target = createdPlayer.transform;
-				hiderLayerFOV.transform.Translate(0, 0, 8);
-				var hiderLayerPeripheral = ((Transform)Instantiate(hiderPeripheral, createdPlayer.transform.position, Quaternion.identity)).gameObject;
-				hiderLayerPeripheral.transform.parent = createdPlayer.transform;
-				hiderLayerPeripheral.GetComponent<LightFollowPlayer>().target = createdPlayer.transform;
-				hiderLayerPeripheral.transform.Translate(0, 0, 8);
-                if (myPlayer == playerData.Value.PlayerID)
-                {
-					// shadow overlay square
-                    var shadows = ((Transform)Instantiate(shadowoverlay, createdPlayer.transform.position, Quaternion.identity)).gameObject;
-                    // set to child of FOV lighting
-					shadows.transform.parent = lightingFOV.transform;
-					// move below everything
-                    shadows.transform.Translate(0, 0, 11);
-                }
+					// These are the FOV & peripheral vision occlusion masks
+					var lightingGunnerFOV = ((Transform)Instantiate(gunnerVision.lightSourceFOV, createdPlayer.transform.position, Quaternion.identity)).gameObject;
+					lightingGunnerFOV.transform.parent = createdPlayer.transform;
+					lightingGunnerFOV.GetComponent<LightFollowPlayer>().target = createdPlayer.transform;
+					lightingGunnerFOV.GetComponent<RotateWithPlayer>().target = createdPlayer.transform;
+					lightingGunnerFOV.transform.Translate(0, 0, 8);
+					
+					var lightingGunnerPeripheral = ((Transform)Instantiate(gunnerVision.lightSourcePeripheral, createdPlayer.transform.position, Quaternion.identity)).gameObject;
+					lightingGunnerPeripheral.transform.parent = createdPlayer.transform;
+					lightingGunnerPeripheral.GetComponent<LightFollowPlayer>().target = createdPlayer.transform;
+					lightingGunnerPeripheral.transform.Translate(0, 0, 8);
+
+					// These are the FOV & peripheral vision stencil masks
+					var hiderLayerGunnerFOV = ((Transform)Instantiate(gunnerVision.hiderFOV, createdPlayer.transform.position, Quaternion.identity)).gameObject;
+					hiderLayerGunnerFOV.transform.parent = createdPlayer.transform;
+					hiderLayerGunnerFOV.GetComponent<LightFollowPlayer>().target = createdPlayer.transform;
+					hiderLayerGunnerFOV.GetComponent<RotateWithPlayer>().target = createdPlayer.transform;
+					hiderLayerGunnerFOV.transform.Translate(0, 0, 8);
+					
+					var hiderLayerGunnerPeripheral = ((Transform)Instantiate(gunnerVision.hiderPeripheral, createdPlayer.transform.position, Quaternion.identity)).gameObject;
+					hiderLayerGunnerPeripheral.transform.parent = createdPlayer.transform;
+					hiderLayerGunnerPeripheral.GetComponent<LightFollowPlayer>().target = createdPlayer.transform;
+					hiderLayerGunnerPeripheral.transform.Translate(0, 0, 8);
+
+					if (myPlayer == playerData.Value.PlayerID)
+					{
+						// shadow overlay square
+						var shadows = ((Transform)Instantiate(shadowoverlay, createdPlayer.transform.position, Quaternion.identity)).gameObject;
+						// set to child of FOV lighting
+						shadows.transform.parent = lightingGunnerFOV.transform;
+						// move below everything
+						shadows.transform.Translate(0, 0, 11);
+					}
+
+					break;
+				case ClassType.Ninja:
+
+					// These are the FOV & peripheral vision occlusion masks
+					var lightingNinjaFOV = ((Transform)Instantiate(ninjaVision.lightSourceFOV, createdPlayer.transform.position, Quaternion.identity)).gameObject;
+					lightingNinjaFOV.transform.parent = createdPlayer.transform;
+					lightingNinjaFOV.GetComponent<LightFollowPlayer>().target = createdPlayer.transform;
+					lightingNinjaFOV.GetComponent<RotateWithPlayer>().target = createdPlayer.transform;
+					lightingNinjaFOV.transform.Translate(0, 0, 8);
+					
+					var lightingNinjaPeripheral = ((Transform)Instantiate(ninjaVision.lightSourcePeripheral, createdPlayer.transform.position, Quaternion.identity)).gameObject;
+					lightingNinjaPeripheral.transform.parent = createdPlayer.transform;
+					lightingNinjaPeripheral.GetComponent<LightFollowPlayer>().target = createdPlayer.transform;
+					lightingNinjaPeripheral.transform.Translate(0, 0, 8);
+					
+					// These are the FOV & peripheral vision stencil masks
+					var hiderLayerNinjaFOV = ((Transform)Instantiate(ninjaVision.hiderFOV, createdPlayer.transform.position, Quaternion.identity)).gameObject;
+					hiderLayerNinjaFOV.transform.parent = createdPlayer.transform;
+					hiderLayerNinjaFOV.GetComponent<LightFollowPlayer>().target = createdPlayer.transform;
+					hiderLayerNinjaFOV.GetComponent<RotateWithPlayer>().target = createdPlayer.transform;
+					hiderLayerNinjaFOV.transform.Translate(0, 0, 8);
+					
+					var hiderLayerNinjaPeripheral = ((Transform)Instantiate(ninjaVision.hiderPeripheral, createdPlayer.transform.position, Quaternion.identity)).gameObject;
+					hiderLayerNinjaPeripheral.transform.parent = createdPlayer.transform;
+					hiderLayerNinjaPeripheral.GetComponent<LightFollowPlayer>().target = createdPlayer.transform;
+					hiderLayerNinjaPeripheral.transform.Translate(0, 0, 8);
+
+					if (myPlayer == playerData.Value.PlayerID)
+					{
+						// shadow overlay square
+						var shadows = ((Transform)Instantiate(shadowoverlay, createdPlayer.transform.position, Quaternion.identity)).gameObject;
+						// set to child of FOV lighting
+						shadows.transform.parent = lightingNinjaFOV.transform;
+						// move below everything
+						shadows.transform.Translate(0, 0, 11);
+					}
+
+					break;
+				case ClassType.Wizard:
+
+					var lightingMagePeripheral = ((Transform)Instantiate(mageVision.lightSourcePeripheral, createdPlayer.transform.position, Quaternion.identity)).gameObject;
+					lightingMagePeripheral.transform.parent = createdPlayer.transform;
+					lightingMagePeripheral.GetComponent<LightFollowPlayer>().target = createdPlayer.transform;
+					lightingMagePeripheral.transform.Translate(0, 0, 8);
+					
+					var hiderLayerMagePeripheral = ((Transform)Instantiate(mageVision.hiderPeripheral, createdPlayer.transform.position, Quaternion.identity)).gameObject;
+					hiderLayerMagePeripheral.transform.parent = createdPlayer.transform;
+					hiderLayerMagePeripheral.GetComponent<LightFollowPlayer>().target = createdPlayer.transform;
+					hiderLayerMagePeripheral.transform.Translate(0, 0, 8);
+
+					if (myPlayer == playerData.Value.PlayerID)
+					{
+						// shadow overlay square
+						var shadows = ((Transform)Instantiate(shadowoverlay, createdPlayer.transform.position, Quaternion.identity)).gameObject;
+						// set to child of FOV lighting
+						shadows.transform.parent = lightingMagePeripheral.transform;
+						// move below everything
+						shadows.transform.Translate(0, 0, 11);
+					}
+
+					break;
+				default:
+					break;
+				}
+
+                
             } else { //Hide enemies
 				// Fetch the stencil masked material
 				Material hiddenMat = (Material)Resources.Load("Stencil_01_Diffuse Sprite", typeof(Material));
