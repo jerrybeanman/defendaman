73e8986b6084ad3ec9c57a12a37ec24dcc1eb305
Hank Lo <Hlo1453@gmail.com>
2016-04-04 10:42:33 -0700

    re-refactored silence to use a flag

--- Assets/Scripts/Effects/Burn.cs
+++ Assets/Scripts/Effects/Burn.cs
@@ -10,7 +10,7 @@ public class Burn : Debuff
     {
         base.Start();
         // .8 scaling ratio based on the attack value of the mage - this attack does true damage, apr4th, hank
-        damage = damage * (float)0.8;
+        damage = damage * (float) 0.8;
         duration = 150;
         if (player == null)
         {
