31604986855c879da6839a94222e28f49477d030
Joseph Tam <josephtam3@hotmail.com>
2016-04-06 18:19:44 -0700

    fix gold

--- Assets/Scripts/Map/Resource.cs
+++ Assets/Scripts/Map/Resource.cs
@@ -218,8 +218,11 @@ class Resource : MonoBehaviour {
     ----------------------------------------------------------------------------------------------------------------------*/
     void SendResourceDepletedMessage(int playerId) {
 		List<Pair<string, string>> msg = CreateResourcePositionMessage();
-        msg.Add(new Pair<string, string>(NetworkKeyString.PlayerID, playerId.ToString()));
-        SendMessageToServer(msg, (int)MapManager.EventType.RESOURCE_DEPLETED);
+        //msg.Add(new Pair<string, string>(NetworkKeyString.PlayerID, playerId.ToString()));
+        if (GameData.MyPlayer.PlayerID == playerId)
+        {
+            SendMessageToServer(msg, (int)MapManager.EventType.RESOURCE_DEPLETED);
+        }
 	}
 
     /*------------------------------------------------------------------------------------------------------------------
