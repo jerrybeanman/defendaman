b7c2fd4cead270635c6d74a1233775dee48b2d7b
Hank Lo <Hlo1453@gmail.com>
2016-04-08 23:36:27 -0700

    Moar commentzz

--- Assets/Scripts/Combat/BasicRanged.cs
+++ Assets/Scripts/Combat/BasicRanged.cs
@@ -1,4 +1,22 @@
-﻿using UnityEngine;
+﻿/*---------------------------------------------------------------------------------------
+--  SOURCE FILE:    BasicRanged.cs
+--
+--  PROGRAM:        Linux Game
+--
+--  FUNCTIONS:
+--
+--  DATE:           April 2, 2016
+--
+--  REVISIONS:      (Date and Description)
+--
+--  DESIGNERS:      Allen Tsang, Hank Lo
+--
+--  PROGRAMMER:     Allen Tsang, Hank Lo
+--
+--  NOTES:
+--  This class is an empty class that other classses inherit from.
+---------------------------------------------------------------------------------------*/
+using UnityEngine;
 using System.Collections;
 using System;
 
