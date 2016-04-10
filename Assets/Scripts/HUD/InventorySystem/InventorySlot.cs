27a7139172f7788f05d2185a0435d4581d6575ea
Joseph Tam <josephtam3@hotmail.com>
2016-04-10 16:38:37 -0700

    added comments to InventorySlots.cs

--- Assets/Scripts/HUD/InventorySystem/InventorySlot.cs
+++ Assets/Scripts/HUD/InventorySystem/InventorySlot.cs
@@ -13,7 +13,7 @@ using System;
 --		public void OnDrop(PointerEventData eventData)
 --      
 -- DATE:		17/02/2016
--- REVISIONS:	(V1.0)
+-- REVISIONS:	
 -- DESIGNER:	Joseph Tam-Huang
 -- PROGRAMMER:  Joseph Tam-Huang
 -----------------------------------------------------------------------------*/
@@ -22,55 +22,62 @@ public class InventorySlot : MonoBehaviour, IDropHandler
     public int slot_pos;
     private Inventory _inventory;
 
-    /*
-     * Retrieves the Inventory script
-     */
+    /*-----------------------------------------------------------------------------------
+    -- FUNCTION: 	Start
+    -- DATE: 		17/02/2016
+    -- REVISIONS: 	
+    -- DESIGNER:  	Joseph Tam-Huang
+    -- PROGRAMMER: 	Joseph Tam-Huang
+    -- INTERFACE: 	void Start()
+    -- RETURNS: 	void
+    -- NOTES:
+    -- Rettrives the Inventory script.
+    ----------------------------------------------------------------------------------*/
     void Start()
     {
         _inventory = GameObject.Find("Inventory").GetComponent<Inventory>();
     }
 
-    /*
-     * Trigger when an inventory item is dragged and dropped to another or the
-     * the same inventory slot.
-     * If dropped to an inventory slot with an item in it, the items swap
-     * slot positions. Otherwise, the item is moved to the new position
-     */
+    /*-------------------------------------------------------------------------------------------------
+    -- FUNCTION: 	OnDrop
+    -- DATE: 		17/02/2016
+    -- REVISIONS: 	
+    -- DESIGNER:  	Joseph Tam-Huang
+    -- PROGRAMMER: 	Joseph Tam-Huang
+    -- INTERFACE: 	public void OnDrop(PointerEventData eventData)
+    --                  PointerEventData eventData: The event payload associated with pointer events.
+    -- RETURNS: 	void
+    -- NOTES:
+    -- Trigger when an inventory item is dragged and dropped to another or the
+    -- the same inventory slot.
+    -- If dropped to an inventory slot with an item in it, the items swap
+    -- slot positions. Otherwise, the item is moved to the new position
+    -------------------------------------------------------------------------------------------------*/
     public void OnDrop(PointerEventData eventData)
     {
         ItemData _dropped_item = eventData.pointerDrag.GetComponent<ItemData>();
 
-        //if (_dropped_item.item_pos != Constants.WEAPON_SLOT && slot_pos != Constants.WEAPON_SLOT) // Uncomment to make weapon slot static
-        //{
         if (_inventory.inventory_item_list[slot_pos].id == -1)
-            {
-                _inventory.inventory_item_list[_dropped_item.item_pos] = new Item();
-                _inventory.inventory_item_list[slot_pos] = _dropped_item.item;
-                _dropped_item.item_pos = slot_pos;
-            }
-            else if (_dropped_item.item_pos != slot_pos)
-            {
-                Transform item = this.transform.GetChild(0);
-
-                _inventory.inventory_item_list[_dropped_item.item_pos] = item.GetComponent<ItemData>().item;
-                _inventory.inventory_item_list[slot_pos] = _dropped_item.item;
-
-                item.GetComponent<ItemData>().item_pos = _dropped_item.item_pos;
-                item.transform.SetParent(_inventory.slot_list[_dropped_item.item_pos].transform);
-                item.transform.position = _inventory.slot_list[_dropped_item.item_pos].transform.position;
-                _dropped_item.item_pos = slot_pos;
-                _dropped_item.transform.SetParent(this.transform);
-                _dropped_item.transform.position = this.transform.position;
-                _inventory.inventory_item_list[_dropped_item.item_pos] = item.GetComponent<ItemData>().item;
-                _inventory.inventory_item_list[slot_pos] = _dropped_item.item;
-            }
-        //}
-        /*
-        String s = "item pos: ";
-        foreach (Item item in Inventory.instance.inventory_item_list)
         {
-            s += item.id + " ";
+            _inventory.inventory_item_list[_dropped_item.item_pos] = new Item();
+            _inventory.inventory_item_list[slot_pos] = _dropped_item.item;
+            _dropped_item.item_pos = slot_pos;
         }
-        Debug.Log(s);*/
+        else if (_dropped_item.item_pos != slot_pos)
+        {
+            Transform item = this.transform.GetChild(0);
+
+            _inventory.inventory_item_list[_dropped_item.item_pos] = item.GetComponent<ItemData>().item;
+            _inventory.inventory_item_list[slot_pos] = _dropped_item.item;
+
+            item.GetComponent<ItemData>().item_pos = _dropped_item.item_pos;
+            item.transform.SetParent(_inventory.slot_list[_dropped_item.item_pos].transform);
+            item.transform.position = _inventory.slot_list[_dropped_item.item_pos].transform.position;
+            _dropped_item.item_pos = slot_pos;
+            _dropped_item.transform.SetParent(this.transform);
+            _dropped_item.transform.position = this.transform.position;
+            _inventory.inventory_item_list[_dropped_item.item_pos] = item.GetComponent<ItemData>().item;
+            _inventory.inventory_item_list[slot_pos] = _dropped_item.item;
+        }
     }
 }
