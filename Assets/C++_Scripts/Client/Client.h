61514be8ba9d2c1c7516d9419873040791902e82
jerry jia <jerry jia>
2016-04-11 19:56:13 -0700

    cleaned up unused scripts and comments

--- Assets/C++_Scripts/Client/Client.h
+++ Assets/C++_Scripts/Client/Client.h
@@ -17,7 +17,24 @@
 #define PACKETLEN    4096
 #define PACKETLEN_UDP 512
 #define MAXPACKETS   100
+/*------------------------------------------------------------------------------
 
+  SOURCE FILE:              Client.h
+
+  PROGRAM:                  Defendaman
+
+  FUNCTIONS:                virtual ~Client() {};
+                            virtual int Init_Client_Socket(const char* name, short port) = 0
+                            virtual int Send(char * message, int size) = 0;
+                            virtual void * Recv() = 0;
+                            void fatal(const char* error);
+                            int Init_SockAddr(const char* hostname, short hostport);
+                            char* GetData();
+
+  DESIGNER/PROGRAMMER:      Jerry Jia, Martin Minkov, Tyler Trepanier, Scott Plummer
+
+  NOTES:                    The client base class for both TCP and UDP network
+-------------------------------------------------------------------------------*/
 namespace Networking
 {
     class Client
