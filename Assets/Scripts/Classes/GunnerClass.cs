4713232f061ce3cfb1b86cf436f8f593c234e85f
jerry jia <jerry1bean@hotmail.com>
2016-05-08 14:21:09 -0700

    fixed tree collider display on tron theme and grass theme

--- Assets/Scripts/Classes/GunnerClass.cs
+++ Assets/Scripts/Classes/GunnerClass.cs
@@ -94,7 +94,7 @@ public class GunnerClass : RangedClass
     ---------------------------------------------------------------------------------------------------------------------*/
 	new void Start()
 	{
-		cooldowns = new float[2] { 0.15f, 8f };
+		cooldowns = new float[2] { 0.25f, 8f };
 
         healthBar = transform.GetChild(0).gameObject.GetComponent<HealthBar>();
         _classStat = new PlayerBaseStat(playerID, healthBar);
